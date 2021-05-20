137d453b521d74648b84980998607dbc59782f24
farahhaddar <farah.haddar99@gmai.com>
2021-05-20 18:56:37 +0300

    abou page

--- Scripts/AboutUs.js
+++ Scripts/AboutUs.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
     $("#brochure").load("Components/AboutUs/Brochure.html");
     $("#Mission_Vission").load("Components/AboutUs/Mission_Vission.html");
     $("#overView").load("Components/AboutUs/OverView.html");
+    $("#projects").load("Components/AboutUs/AboutProjects.html");
+    $("#objectives").load("Components/AboutUs/Objectives.html");
+    $("#partners").load("Components/AboutUs/Partners.html");
+
+
 });
